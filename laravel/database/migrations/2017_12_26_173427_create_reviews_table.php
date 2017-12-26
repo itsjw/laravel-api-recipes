337639d71232d97dce3a2e7a49f3135e96e50ff0
Hiren <hirenbudhrani@gmail.com>
2017-12-26 22:36:02 +0100

    base classes for recipes app

---
+++ laravel/database/migrations/2017_12_26_173427_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('chef_id')->unsigned();
+            $table->integer('recipe_id')->unsigned();
+            $table->char('grade');
+            $table->text('review');
+            $table->timestamps();
+
+            $table->foreign('chef_id')->references('id')->on('chefs');
+            $table->foreign('recipe_id')->references('id')->on('recipes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
